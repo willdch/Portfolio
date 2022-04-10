14c72d7e5ee3bc0dad693911b9b65777f248d55e
Will Harris <will@lifenet.net>
2022-04-09 23:12:08 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Header from './Header';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      
+      <Router>
+        <Routes>
+          {/* <Route path="header" element={<Header/>} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+
+
+const header = (props) => {
+ return (
+   <div>
+     <p>This page works</p>
+   </div>
+ )
+}
+
+export default header;
